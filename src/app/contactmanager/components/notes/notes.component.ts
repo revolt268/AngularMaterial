eaddee2458ac3327455ff0725f966e7ce7f9fa65
revolt268 <revolt268@charter.net>
2021-06-01 09:32:21 -0500

    Added Table for notes iwth pagination and sorting

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,8 @@
+import { AfterViewInit, ViewChild } from '@angular/core';
 import { Component, Input, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
 @Component({
@@ -6,13 +10,31 @@ import { Note } from '../../models/note';
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent implements OnInit, AfterViewInit {
 
   @Input()
   notes!: Note[];
+
+  displayedColumns: string[] = ['position', 'title', 'date'];
+  dataSource!: MatTableDataSource<Note>;
+
   constructor() { }
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
