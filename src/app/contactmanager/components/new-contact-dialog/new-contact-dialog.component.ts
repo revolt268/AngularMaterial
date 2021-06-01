452087b953d3532c8a0bf7fe7d413af8d520889d
revolt268 <revolt268@charter.net>
2021-06-01 11:04:47 -0500

    Adding new user to list

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-new-contact-dialog',
@@ -14,7 +15,8 @@ export class NewContactDialogComponent implements OnInit {
     'svg-1', 'svg-2', 'svg-3', 'svg-4'
   ];
   user!: User;
-  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>,
+              private userService: UserService) { }
 
   name = new FormControl('', [Validators.required]);
 
@@ -28,7 +30,10 @@ export class NewContactDialogComponent implements OnInit {
 
   save() {
     this.user.name = this.name.value;
-    this.dialogRef.close(this.user);
+
+    this.userService.addUser(this.user).then(user => {
+      this.dialogRef.close(user);
+    });
   }
 
   dismiss() : void {
